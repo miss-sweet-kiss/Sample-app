8a1a23f0de822e63cd9057c3ffc6b69e1ccf79d1
miss-sweet-kiss <nyuto4ka12@rambler.ru>
2014-07-25 16:56:13 +0300

    Devise_2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'desolate-castle-8582.herokuapp.com', port: 5000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 =>  587,
+      :user_name            => 'tiptip3000@gmail.com',
+      :password             => 'asdfasdfasdf',
+      :domain               => 'some.org',
+      :authentication       => :plain,
+      #:openssl_verify_mode  => 'none',
+      :enable_starttls_auto => true
+  }
 end
